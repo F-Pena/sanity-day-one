8fe9c837c8f1d3ac6f025118f0937e0b85eece7a
Fernando Pena <fpcasiano@hotmail.com>
2026-05-31 00:58:58 -0500

    Enhance migration script to replace eventType with format
    - Updated migration logic to include an idempotence key, preventing duplicate migrations.
    - Added a filter to ensure only relevant documents are processed.
    - Set the _migrations field to track migration status, improving migration management.

--- apps/studio/migrations/replace-event-type-with-event-format/index.ts
+++ apps/studio/migrations/replace-event-type-with-event-format/index.ts
@@ -1,17 +1,24 @@
-import {defineMigration, at, setIfMissing, unset} from 'sanity/migrate'
+import {defineMigration, at, setIfMissing, unset, insert} from 'sanity/migrate'
 
-const from = 'eventType'
-const to = 'format'
+const from = 'eventType';
+const to = 'format';
+const idempotenceKey = 'xyz';
 
 export default defineMigration({
   title: "Replace event type with event format",
   documentTypes: ["event"],
-
+  filter: '_type == "event" && defined(eventType) && !defined(format)',
   migrate: {
     document(doc, context) {
+      if((doc?._migrations as string[] || []).includes(idempotenceKey)) {
+        return;
+      }
+
       return [
         at(to, setIfMissing(doc[from])),
-        at(from, unset())
+        at(from, unset()),
+        at('_migrations', setIfMissing([])),
+        at('_migrations', insert(idempotenceKey, 'after', 0)),
       ]
     }
   }
